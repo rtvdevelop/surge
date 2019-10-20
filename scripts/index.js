b5f44dc1c8061c6d42d13bbc5406389cba9029f4
rtvdevelop <valluriravitej@gmail.com>
2019-10-20 21:35:11 +0530

    Slide 2 added in carousel

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,12 @@
 $(document).ready(function () {
-    $('.carousel').carousel('pause');
     let rating_given = 0;
     let stars_list = null;
     let max_star_rating = 10;
-    const target = document.querySelector('.rating-wrapper');
     HTMLSpanElement.prototype.reset = function () {
         this.classList.remove('checked');
     }
     HTMLSpanElement.prototype.highlight = function () {
         this.classList.add('checked');
-    }
-    function getStarNode(id_name, class_name) {
-        const star_elm = document.createElement('span');
-        star_elm.id = id_name;
-        star_elm.className = class_name;
-        return star_elm;
-    }
-    function add(target, elm) {
-        target.append(elm);
     }
     function highlightStarsUpto(node) {
         stars_list = document.querySelectorAll('.rating-wrapper .fa.fa-star');
@@ -37,34 +26,48 @@ $(document).ready(function () {
             })
         }
     }
-    function populateMaxRating() {
-        document.querySelector('#max-value').innerHTML = max_star_rating;
+    function populateMaxRating(targetNode) {
+        targetNode.parentElement.children[2].children[1].innerHTML = max_star_rating;
     }
-    function populateGivenRating(value) {
-        document.querySelector('#rated-value').innerHTML = value;
+    function populateGivenRating(targetNode, value) {
+        targetNode.parentElement.children[2].children[0].innerHTML = value;
     }
     function init() {
-        populateMaxRating();
-        populateGivenRating(rating_given);
-        let i = 1;
-        while (i <= max_star_rating) {
-            const node = getStarNode(i, 'fa fa-star');
-            node.addEventListener('click', node => {
-                rating_given = node.target.id;
-                highlightStarsUpto(node);
-                populateGivenRating(rating_given);
-            })
-            node.addEventListener('mouseover', node => {
-                highlightStarsUpto(node);
-                populateGivenRating(node.target.id);
-            })
-            node.addEventListener('mouseout', node => {
-                resetRating();
-                populateGivenRating(rating_given);
-            })
-            add(target, node);
-            i++;
-        };
+        const targetList = document.querySelectorAll('.rating-wrapper');
+        function getStarNode(id_name, class_name) {
+            const star_elm = document.createElement('span');
+            star_elm.id = id_name;
+            star_elm.className = class_name;
+            return star_elm;
+        }
+        function add(target, elm) {
+            target.append(elm);
+        }
+        targetList.forEach(targetNode => {
+            populateMaxRating(targetNode);
+            populateGivenRating(targetNode, rating_given);
+            let i = 1;
+            while (i <= max_star_rating) {
+                const node = getStarNode(i, 'fa fa-star');
+                node.addEventListener('click', node => {
+                    rating_given = node.target.id;
+                    highlightStarsUpto(node);
+                    populateGivenRating(targetNode, rating_given);
+                })
+                node.addEventListener('mouseover', node => {
+                    highlightStarsUpto(node);
+                    populateGivenRating(targetNode, node.target.id);
+                })
+                node.addEventListener('mouseout', node => {
+                    resetRating();
+                    populateGivenRating(targetNode, rating_given);
+                })
+                add(targetNode, node);
+                i++;
+            };
+        })
     }
     init();
+
+
 })
