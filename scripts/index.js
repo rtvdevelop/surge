da073eafe0ce3f2ab019d8c29de27a443fe62349
rtvdevelop <valluriravitej@gmail.com>
2019-10-22 23:10:38 +0530

    rating bar hover issue fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    let rating_given = 0;
-    let stars_list = null;
+    let rating_given_slide1 = 0;
+    let rating_given_slide2 = 0;
     let max_star_rating = 10;
     HTMLSpanElement.prototype.reset = function () {
         this.classList.remove('checked');
@@ -9,28 +9,46 @@ $(document).ready(function () {
         this.classList.add('checked');
     }
     function highlightStarsUpto(node) {
-        stars_list = document.querySelectorAll('.rating-wrapper .fa.fa-star');
-        stars_list.forEach(star => {
+        node.parentNode.childNodes.forEach(star => {
             star.reset();
-            if (parseInt(star.id) <= parseInt(node.target.id)) {
+            if (parseInt(star.id) <= parseInt(node.id)) {
                 star.highlight();
             }
         })
     }
-    function resetRating() {
-        if (rating_given) {
-            highlightStarsUpto({ target: { id: rating_given } });
-        } else {
-            stars_list.forEach(star => {
-                star.reset();
-            })
+    function resetRating(node) {
+        const context_is_slide1 = node.target.parentElement.classList.contains('slide1');
+        const context_is_slide2 = node.target.parentElement.classList.contains('slide2');
+        if (rating_given_slide1 > 0 && context_is_slide1) {
+            const currNode = document.querySelector('.slide-1 .rating-wrapper').children[rating_given_slide1 - 1];
+            highlightStarsUpto(currNode);
+            return;
         }
+        if (rating_given_slide2 > 0 && context_is_slide2) {
+            const currNode = document.querySelector('.slide-2 .rating-wrapper').children[rating_given_slide2 - 1];
+            highlightStarsUpto(currNode);
+            return;
+
+        }
+        node.target.parentNode.childNodes.forEach(star => {
+            star.reset();
+        })
+
     }
     function populateMaxRating(targetNode) {
         targetNode.parentElement.children[2].children[1].innerHTML = max_star_rating;
     }
     function populateGivenRating(targetNode, value) {
         targetNode.parentElement.children[2].children[0].innerHTML = value;
+    }
+
+    function getRatingWrapperContext(node) {
+        if (node.target.parentNode.classList.contains('slide1')) {
+            return 1;
+        } else {
+            return 2;
+        }
+
     }
     function init() {
         const targetList = document.querySelectorAll('.rating-wrapper');
@@ -45,22 +63,25 @@ $(document).ready(function () {
         }
         targetList.forEach(targetNode => {
             populateMaxRating(targetNode);
-            populateGivenRating(targetNode, rating_given);
+            populateGivenRating(targetNode, 0);
             let i = 1;
             while (i <= max_star_rating) {
                 const node = getStarNode(i, 'fa fa-star');
                 node.addEventListener('click', node => {
-                    rating_given = node.target.id;
-                    highlightStarsUpto(node);
-                    populateGivenRating(targetNode, rating_given);
-                })
-                node.addEventListener('mouseover', node => {
-                    highlightStarsUpto(node);
+                    let context = getRatingWrapperContext(node);
+                    if (context === 1) {
+                        rating_given_slide1 = parseInt(node.target.id);
+                    } else {
+                        rating_given_slide2 = parseInt(node.target.id);
+                    }
+                    highlightStarsUpto(node.target);
                     populateGivenRating(targetNode, node.target.id);
                 })
+                node.addEventListener('mouseover', node => {
+                    highlightStarsUpto(node.target);
+                })
                 node.addEventListener('mouseout', node => {
-                    resetRating();
-                    populateGivenRating(targetNode, rating_given);
+                    resetRating(node);
                 })
                 add(targetNode, node);
                 i++;
@@ -68,6 +89,4 @@ $(document).ready(function () {
         })
     }
     init();
-
-
 })
